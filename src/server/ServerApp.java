a0c502009ba5210166434f582604ec5b997f5192
Florian Lardy <florianlardy@hotmail.com>
2018-11-05 13:45:36 +0100

    Correction affichage nom item ajouté

--- src/server/ServerApp.java
+++ src/server/ServerApp.java
@@ -83,7 +83,7 @@ public class ServerApp extends UnicastRemoteObject implements IServer {
 
 	@Override
 	public void submit(Item item) throws RemoteException {
-		System.out.println("New item registered : " + item);
+		System.out.println("New item registered : " + item.getName() + " by " + item.getSeller());
 		this.items.add(item);
 		dbManager.addItem(item);
 		for (IClient c : clients) {
